c99af3c9a982fc53e84d1588462bfbbc01c0d6b1
west1989 <gigiabakelia@gmail.com>
2020-07-04 00:58:10 +0400

    Register and Login

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authSerice: AuthService) {}
+
+  ngOnInit(): void {}
+
+  login() {
+    this.authSerice.login(this.model).subscribe(
+      (next) => {
+        console.log('Logged in successfully');
+      },
+      (error) => {
+        console.log('Failed to login');
+      }
+    );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+
+}
