98d22c9b5a4a8e209f4c97dd327e87026df43a3f
west1989 <gigiabakelia@gmail.com>
2020-07-14 00:35:48 +0400

    Pagination, Filters, Order

--- src/app/_resolvers/member-list.resolver.ts
+++ src/app/_resolvers/member-list.resolver.ts
@@ -12,11 +12,13 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class MemberListResolver implements Resolve<User[]> {
+  pageNumber = 1;
+  pageSize = 5;
 
   constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<User[]> {
-    return this.userService.getUsers()
+    return this.userService.getUsers(this.pageNumber, this.pageSize)
     .pipe(catchError(error => {
       this.alertify.error('Problem retrieving data');
       this.router.navigate(['/home']);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { TabsModule } from 'ngx-bootstrap/tabs';
@@ -56,6 +58,8 @@ export function tokenGetter() {
     ReactiveFormsModule,
     BsDropdownModule.forRoot(),
     BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot(),
+    ButtonsModule.forRoot(),
     TabsModule.forRoot(),
     RouterModule.forRoot(appRoutes),
     NgxGalleryModule,
